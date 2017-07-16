8d433bc5bf3ea1aef2025eae32281a8f325808d0
Murat Gunana <mgunana@gmail.com>
2017-07-17 00:23:47 +0100

    Object coordinates data moved to config file.

--- OpenNI2SkeletonData.cpp
+++ OpenNI2SkeletonData.cpp
@@ -2,11 +2,14 @@
 #include <yarp/os/BufferedPort.h>
 #include <yarp/os/Bottle.h>
 #include <yarp/sig/all.h>
+#include <yarp/os/Property.h>
+
 #include <yarp/sig/Vector.h>
 #include <yarp/sig/Matrix.h>
 #include <yarp/math/api.h>
 #include <yarp/math/Quaternion.h>
 #include <yarp/math/Math.h>
+
 #include <iostream>
 #include <map>
 #include <stdio.h>
@@ -70,6 +73,8 @@ int main(int argc, char* argv[]) {
   Network::connect("/OpenNI2/imageFrame:o","/imagergb");
   Network::connect("/OpenNI2/userSkeleton:o","/receiver");
   Vector elbow_joint(3), wrist_joint(3), hand_vector(3);
+  yarp::os::Property prop;
+  
   while(true) {
     Bottle *bot = input.read();
     ImageOf<PixelRgb> *image = imagePort.read();
@@ -115,65 +120,54 @@ int main(int argc, char* argv[]) {
       printf("hand vector normalized: %s\n", hand_vector.toString().c_str());
       // Now we need to create ball centre point as vector.
       Vector ball_center(3), red_ball(3), purple_ball(3);
+       
+      prop.fromConfigFile("object.ini");
+      Bottle envBottle;
+      envBottle = prop.findGroup("home");
       
-      // Home ball position coordinates.
-       //ball_center[0] = 0.0;
-       //ball_center[1] = -27.5; 
-       //ball_center[2] = 40.0;
-    
-      //red_ll[0] = 0.0;
-      //red_ball[1] = -23.0; 
-      //red_ball[2] = 40.0;
+      red_ball[0] = envBottle.findGroup("red").get(1).asDouble();
+      red_ball[1] = envBottle.findGroup("red").get(2).asDouble(); 
+      red_ball[2] = envBottle.findGroup("red").get(3).asDouble();
 
-      //purple_ball[0] = -30.0;
-      //purple_ball[1] = -23.0;
-      //purple_ball[2] = 40.0;
-      
-      // Robotics lab ball position coordinates.
-      red_ball[0] = 26.0;
-      red_ball[1] = -20.0; 
-      red_ball[2] = 43.5;
-
-      purple_ball[0] = -8.5;
-      purple_ball[1] = -20.0;
-      purple_ball[2] = 43.5;
+      purple_ball[0] = envBottle.findGroup("purple").get(1).asDouble();
+      purple_ball[1] = envBottle.findGroup("purple").get(2).asDouble();
+      purple_ball[2] = envBottle.findGroup("purple").get(3).asDouble();
      
-      
       VectorOf<Vector> vector_storage(2);
       //vector_storage[1] = red_ball;
       //vector_storage[2] = purple_ball;
       
       for (int i = 0; i < vector_storage.size(); ++i) {
         // Create another vector from elbow joint to ball center.
-      // This is needed to get dot product of hand_vector.
-      if (i == 0) ball_center = red_ball;
-      else
-        ball_center = purple_ball;
-      //printf("Vector storage: %s\n", vector_storage[i].toString().c_str());
-      //ball_center = vector_storage[i];
-      Vector elbow_ball_vector(3);
-      elbow_ball_vector = ball_center - elbow_joint;
+        // This is needed to get dot product of hand_vector.
+        if (i == 0) ball_center = red_ball;
+        else
+          ball_center = purple_ball;
+        //printf("Vector storage: %s\n", vector_storage[i].toString().c_str());
+        //ball_center = vector_storage[i];
+        Vector elbow_ball_vector(3);
+        elbow_ball_vector = ball_center - elbow_joint;
 
   
-      // We need to take dot product of hand_vector with elbow_ball_vector 
-      // and this will give us the distance. If the result is less than zero then the hand vector
-      // is pointing away from the ball.
-      double distance, radius = 4.5;
-      distance = dot(hand_vector, elbow_ball_vector);
+        // We need to take dot product of hand_vector with elbow_ball_vector 
+        // and this will give us the distance. If the result is less than zero then the hand vector
+        // is pointing away from the ball.
+        double distance, radius = envBottle.find("radius").asDouble();
+        distance = dot(hand_vector, elbow_ball_vector);
       
-      // Scale hand vector in order to calculate the closest point to the ball.
-      Vector hand_vector_scaled(3), closest_point(3);
-      hand_vector_scaled = hand_vector * distance;
-      closest_point = elbow_joint + hand_vector_scaled;
+        // Scale hand vector in order to calculate the closest point to the ball.
+        Vector hand_vector_scaled(3), closest_point(3);
+        hand_vector_scaled = hand_vector * distance;
+        closest_point = elbow_joint + hand_vector_scaled;
       
-      // Check the distance between the closest point and object center. If it is smaller than 
-      // the radius then the points is inside the object. That means that the hand points the object with 
-      // correct coordinates. 
-      printf("Distance: %f\n", distance);      
-      printf("D2: %f\n", dot(ball_center - closest_point, ball_center - closest_point));
-      if (distance > 0 && dot(ball_center - closest_point, ball_center - closest_point) < radius * radius) {
-        printf("Pointing correctly: %f\n", distance);
-      }
+        // Check the distance between the closest point and object center. If it is smaller than 
+        // the radius then the points is inside the object. That means that the hand points the object with 
+        // correct coordinates. 
+        printf("Distance: %f\n", distance);      
+        printf("D2: %f\n", dot(ball_center - closest_point, ball_center - closest_point));
+        if (distance > 0 && dot(ball_center - closest_point, ball_center - closest_point) < radius * radius) {
+          printf("Pointing correctly: %f\n", distance);
+        }
       }
     }
     //printf("Position: %sa\n", bot->toString().c_str());
--- build/CMakeFiles/FindLocation.dir/DependInfo.cmake
+++ build/CMakeFiles/FindLocation.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/robotics/thesis/FindLocation.cpp" "/home/robotics/thesis/build/CMakeFiles/FindLocation.dir/FindLocation.cpp.o"
+  "/home/muri/thesis/FindLocation.cpp" "/home/muri/thesis/build/CMakeFiles/FindLocation.dir/FindLocation.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
